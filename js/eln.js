0f01ab6641011820007daf102625abde9fc12683
na3im <naimghost06@gmail.com>
2021-01-29 12:34:22 +0100

    Update eln.js

--- js/eln.js
+++ js/eln.js
@@ -1,1 +1,1 @@
-$('a#mob').each(function(){ this.href=this.href.replace('https://docs.google.com/open?id=', 'https://drive.google.com/open?id=')});
+$('a#mhob').each(function(){ this.href=this.href.replace('https://docs.google.com/open?id=', 'https://drive.google.com/open?id=')});
