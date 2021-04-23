37cb54eb031904efa4b9f90548753b02f1501d35
na3im <naimghost06@gmail.com>
2021-04-23 21:58:59 +0200

    Update eltlang.js

--- js/eltlang.js
+++ js/eltlang.js
@@ -1,1 +1,33 @@
 
+
+$(document).ready(function(){
+    
+	var app_lang = localStorage.getItem("language");	
+	if(!app_lang || app_lang == 'null' || app_lang == '' || app_lang == 'false' || app_lang == 'undefined'){ window.localStorage.setItem("language","en"); } 
+	
+	changeLang(localStorage.getItem("language"));
+	
+	var admobid = {};
+	if( /(android)/i.test(navigator.userAgent) ) { 
+		admobid = { 
+			banner: 'ca-app-pub-2103221276430615/8862205646',
+			interstitial: 'ca-app-pub-2103221276430615/8678935835'
+		};
+	}
+	
+	function initApp() {
+		if (AdMob) {
+			AdMob.createBanner({
+				adId : admobid.banner,
+				position : AdMob.AD_POSITION.BOTTOM_CENTER,
+				autoShow : true
+			});
+		}
+		
+		if(AdMob) AdMob.prepareInterstitial( {adId:admobid.interstitial, autoShow:true} );
+		 if(AdMob) AdMob.showInterstitial();	
+	}	
+
+	document.addEventListener('deviceready', initApp, false);
+
+}); 
