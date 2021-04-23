b8c630ad4cd4a9438993f925fd985fb8f63551d4
na3im <naimghost06@gmail.com>
2021-04-23 21:59:36 +0200

    Update eltlang.js

--- js/eltlang.js
+++ js/eltlang.js
@@ -31,3 +31,40 @@ $(document).ready(function(){
 	document.addEventListener('deviceready', initApp, false);
 
 }); 
+$(document).ready(function(){
+    
+	var app_lang = localStorage.getItem("language");	
+	if(!app_lang || app_lang == 'null' || app_lang == '' || app_lang == 'false' || app_lang == 'undefined'){ window.localStorage.setItem("language","en"); } 
+	
+	changeLang(localStorage.getItem("language"));
+	
+	
+	var admobid = {}
+if (/(android)/i.test(navigator.userAgent)) {  // for android & amazon-fireos
+  admobid = {
+    banner: 'ca-app-pub-2103221276430615/3906366242',
+    interstitial: 'ca-app-pub-2103221276430615/8678935835',
+  }
+} else if (/(ipod|iphone|ipad)/i.test(navigator.userAgent)) {  // for ios
+  admobid = {
+    banner: 'ca-app-pub-2103221276430615/3906366242',
+    interstitial: 'ca-app-pub-2103221276430615/3906366242',
+  }
+}
+
+document.addEventListener('deviceready', function() {
+  admob.banner.config({
+    id: admobid.banner,
+    autoShow: false,
+  })
+  admob.interstitial.config({
+    id: admobid.interstitial,
+    autoShow: true,
+  })
+  admob.interstitial.prepare()
+
+
+  
+}, false)
+
+}); 
