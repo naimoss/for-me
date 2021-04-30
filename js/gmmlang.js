9f6f3275bc4de2ecf7fc7d57f490ebbb54b99af2
na3im <naimghost06@gmail.com>
2021-05-01 00:03:27 +0200

    Update gmmlang.js

--- js/gmmlang.js
+++ js/gmmlang.js
@@ -1,3 +1,4 @@
+
 function changeLang(lang_string) {
     
 	
@@ -634,6 +635,38 @@ function changeLang(lang_string) {
 }
 
 
+$(document).ready(function(){
+    
+	var app_lang = localStorage.getItem("language");	
+	if(!app_lang || app_lang == 'null' || app_lang == '' || app_lang == 'false' || app_lang == 'undefined'){ window.localStorage.setItem("language","en"); } 
+	
+	changeLang(localStorage.getItem("language"));
+	
+	
+	var admobid = {}
+if (/(android)/i.test(navigator.userAgent)) {  // for android & amazon-fireos
+  admobid = {
+    banner: 'ca-app-pub-2103221276430615/8862205646',
+    interstitial: 'ca-app-pub-2103221276430615/8678935835',
+  }
+} else if (/(ipod|iphone|ipad)/i.test(navigator.userAgent)) {  // for ios
+  admobid = {
+    banner: 'ca-app-pub-2103221276430615/8678935835',
+    interstitial: 'ca-app-pub-2103221276430615/3906366242',
+  }
+}
+
+document.addEventListener('deviceready', function() {
+
+
+  admob.interstitial.config({
+    id: admobid.interstitial,
+    autoShow: true,
+  })
+  admob.interstitial.prepare()  
+}, false)
+
+}); 
 $(document).ready(function(){
     
 	var app_lang = localStorage.getItem("language");	
