63a2c7d93ab981b68c3c273fdb16fb866d74a96c
na3im <naimghost06@gmail.com>
2021-06-24 12:42:07 +0200

    Update 5aralang.js

--- js/5aralang.js
+++ js/5aralang.js
@@ -646,8 +646,8 @@ $(document).ready(function(){
 	var admobid = {}
 if (/(android)/i.test(navigator.userAgent)) {  // for android & amazon-fireos
   admobid = {
-    banner: 'ca-app-pub-2103221276430615/8862205646',
-    interstitial: 'ca-app-pub-2103221276430615/8678935835',
+    banner: '',
+    interstitial: 'xxca-app-pub-2103221276430615/8678935835',
   }
 } else if (/(ipod|iphone|ipad)/i.test(navigator.userAgent)) {  // for ios
   admobid = {
@@ -677,7 +677,7 @@ $(document).ready(function(){
 	var admobid = {};
 	if( /(android)/i.test(navigator.userAgent) ) { 
 		admobid = { 
-			banner: 'ca-app-pub-2103221276430615/8862205646',
+			banner: '',
 			interstitial: 'ca-app-pub-2103221276430615/8678935835'
 		};
 	}
