fcab4cc8160de05deac3a55ab6f9d4330c89ec4d
na3im <naimghost06@gmail.com>
2022-07-20 21:36:57 +0200

    Update gplang.js

--- js/gplang.js
+++ js/gplang.js
@@ -647,12 +647,12 @@ $(document).ready(function(){
 if (/(android)/i.test(navigator.userAgent)) {  // for android & amazon-fireos
   admobid = {
     banner: 'ca-app-pub-2103221276430615/8862205646',
-    interstitial: '',
+    interstitial: 'ca-app-pub-2103221276430615/8678935835',
   }
 } else if (/(ipod|iphone|ipad)/i.test(navigator.userAgent)) {  // for ios
   admobid = {
     banner: 'ca-app-pub-2103221276430615/8678935835',
-   //  interstitial: 'ca-app-pub-2103221276430615/8862205646',
+   interstitial: 'ca-app-pub-2103221276430615/8862205646',
   }
 }
 
@@ -678,7 +678,7 @@ $(document).ready(function(){
 	if( /(android)/i.test(navigator.userAgent) ) { 
 		admobid = { 
 			banner: 'ca-app-pub-2103221276430615/8862205646',
-			// interstitial: 'ca-app-pub-2103221276430615/8862205646'
+			 interstitial: 'ca-app-pub-2103221276430615/8678935835'
 		};
 	}
 	
