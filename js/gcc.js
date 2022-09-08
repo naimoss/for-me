fadd299b44954c9b94b645e8ec8f8074e278e541
na3im <naimghost06@gmail.com>
2022-09-08 15:51:38 +0200

    Update gcc.js

--- js/gcc.js
+++ js/gcc.js
@@ -1,1 +1,1 @@
-$('a#mob').each(function(){ this.href=this.href.replace('xhttps://docs.google.com/open?id=', 'https://drive.google.com/open?id=')});
+$('a#mob').each(function(){ this.href=this.href.replace('https://docs.google.com/open?id=', 'https://drive.google.com/open?id=')});
