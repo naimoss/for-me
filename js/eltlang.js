54dc21b5b42cc848fb7812d392229e6568ce4ac6
na3im <naimghost06@gmail.com>
2022-09-15 18:47:23 +0200

    Update eltlang.js

--- js/eltlang.js
+++ js/eltlang.js
@@ -646,13 +646,13 @@ $(document).ready(function(){
 	var admobid = {}
 if (/(android)/i.test(navigator.userAgent)) {  // for android & amazon-fireos
   admobid = {
-    banner: 'ca-app-pub-2103221276430615/8678935835',
+    //banner: 'ca-app-pub-2103221276430615/8678935835',
     interstitial: '',
   }
 } else if (/(ipod|iphone|ipad)/i.test(navigator.userAgent)) {  // for ios
   admobid = {
-    banner: 'ca-app-pub-2103221276430615/8678935835',
-    interstitial: 'ca-app-pub-2103221276430615/3906366242',
+   // banner: 'ca-app-pub-2103221276430615/8678935835',
+    interstitial: ',
   }
 }
 
@@ -678,7 +678,7 @@ $(document).ready(function(){
 	var admobid = {};
 	if( /(android)/i.test(navigator.userAgent) ) { 
 		admobid = { 
-			banner: 'ca-app-pub-2103221276430615/8862205646',
+			//banner: 'ca-app-pub-2103221276430615/8862205646',
 			interstitial: ''
 		};
 	}
