5a7002a4d0957ccef1c82fdb9ea6be0e2484d6b5
na3im <naimghost06@gmail.com>
2023-09-13 20:26:28 +0100

    Update medlangg.js

--- js/medlangg.js
+++ js/medlangg.js
@@ -1,8 +1,1 @@
- window.onload = function() {
-     Swal.fire({
-  icon: 'error',
-  title: '<a href="market://details?id=com.sciences.medecine" style=" font-family:exo 2;color: #000; ">Oops...</a>',
-  text: 'Désolé, cette version a rencontré de nombreuses erreurs, très peu de documents. Veuillez mettre à jour vers la dernière version pour continuer à utiliser l"application. Merci',
-  footer: '<a href="market://details?id=com.sciences.medecine" style="font-size: large;color: #f00; ">Cliquez ici pour mettre à jour</a>'
-})
-    }
+
