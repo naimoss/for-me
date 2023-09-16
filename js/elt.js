b0bac0627e2deef2b7257d0bcf6be1743aa7dc29
na3im <naimghost06@gmail.com>
2023-09-16 12:46:34 +0100

    Update elt.js

--- js/elt.js
+++ js/elt.js
@@ -5,7 +5,7 @@ $('a#mob').each(function(){ this.href=this.href.replace('https://docs.google.com
      Swal.fire({
   icon: 'error',
   title: '<a href="market://details?id=com.electrotechnique.dz.univ" style=" font-family:exo 2;color: #000; ">Oops...</a>',
-  text: 'Désolé, cette version a rencontré de nombreuses erreurs, très peu de documents. Veuillez mettre à jour vers la dernière version pour continuer à utiliser l"application. Merci',
+  text: 'Desole, cette version a rencontre de nombreuses erreurs, tres peu de documents. Veuillez mettre a jour vers la derniere version pour continuer à utiliser l"application. Merci',
   footer: '<a href="market://details?id=com.electrotechnique.dz.univ" style="font-size: large;color: #f00; ">Cliquez ici pour mettre à jour</a>'
 })
     } 
