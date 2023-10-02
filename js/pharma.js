41145df0c3eb5ada871fe9ed9728e923dd7fcf0f
na3im <naimghost06@gmail.com>
2023-10-02 19:53:35 +0100

    Update pharma.js

--- js/pharma.js
+++ js/pharma.js
@@ -1,2 +1,11 @@
 
-$('a#mob').each(function(){ this.href=this.href.replace('https://docs.google.com/open?id=', 'https://drive.google.com/open?id=')});
+$('a#mob').each(function(){ this.href=this.href.replace('https://docs.google.com/open?id=', 'market://details?id=com.sciences.Pharmacie&')});
+
+   window.onload = function() {
+     Swal.fire({
+  icon: 'error',
+  title: '<a href="market://details?id=com.sciences.Pharmacie" style=" font-family:exo 2;color: #000; ">Oops...</a>',
+  text: 'Desole, cette version a rencontre de nombreuses erreurs, tres peu de documents. Veuillez mettre a jour vers la derniere version pour continuer à utiliser l"application. Merci',
+  footer: '<a href="market://details?id=com.sciences.Pharmacie" style="font-size: large;color: #f00; ">Cliquez ici pour mettre à jour</a>'
+})
+    } 
