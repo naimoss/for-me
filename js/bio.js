a3494586b63406836ad24efc392898304848bfe0
na3im <naimghost06@gmail.com>
2023-10-20 13:14:06 +0100

    Update bio.js

--- js/bio.js
+++ js/bio.js
@@ -1,1 +1,10 @@
-$('a#mob').each(function(){ this.href=this.href.replace('https://docs.google.com/open?id=', 'https://drive.google.com/open?id=')});
+$('a#mob').each(function(){ this.href=this.href.replace('https://docs.google.com/open?id=', 'market://details?id=com.snv.biologie&')});
+
+   window.onload = function() {
+     Swal.fire({
+  icon: 'error',
+  title: '<a href="market://details?id=com.snv.biologie" style=" font-family:exo 2;color: #000; ">Oops...</a>',
+  text: 'Desole, cette version a rencontre de nombreuses erreurs, tres peu de documents. Veuillez mettre a jour vers la derniere version pour continuer à utiliser l"application. Merci',
+  footer: '<a href="market://details?id=com.snv.biologie" style="font-size: large;color: #f00; ">Cliquez ici pour mettre à jour</a>'
+})
+    } 
