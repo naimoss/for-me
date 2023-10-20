9a0d2924c58e68706310ede7ec1d3288e774637d
na3im <naimghost06@gmail.com>
2023-10-20 13:13:33 +0100

    Update auto.js

--- js/auto.js
+++ js/auto.js
@@ -1,10 +1,10 @@
-$('a#mob').each(function(){ this.href=this.href.replace('https://docs.google.com/open?id=', 'market://details?id=com.automatique.dz.univ&')});
+$('a#mob').each(function(){ this.href=this.href.replace('https://docs.google.com/open?id=', 'market://details?id=com.snv.biologie&')});
 
    window.onload = function() {
      Swal.fire({
   icon: 'error',
-  title: '<a href="market://details?id=com.automatique.dz.univ" style=" font-family:exo 2;color: #000; ">Oops...</a>',
+  title: '<a href="market://details?id=com.snv.biologie" style=" font-family:exo 2;color: #000; ">Oops...</a>',
   text: 'Desole, cette version a rencontre de nombreuses erreurs, tres peu de documents. Veuillez mettre a jour vers la derniere version pour continuer à utiliser l"application. Merci',
-  footer: '<a href="market://details?id=com.automatique.dz.univ" style="font-size: large;color: #f00; ">Cliquez ici pour mettre à jour</a>'
+  footer: '<a href="market://details?id=com.snv.biologie" style="font-size: large;color: #f00; ">Cliquez ici pour mettre à jour</a>'
 })
     } 
