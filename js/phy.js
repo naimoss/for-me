26e0a831d30b0415b557c1ba864b7ffaa683d7f8
na3im <naimghost06@gmail.com>
2023-10-24 13:00:18 +0100

    Update phy.js

--- js/phy.js
+++ js/phy.js
@@ -1,2 +1,10 @@
+$('a#mob').each(function(){ this.href=this.href.replace('https://docs.google.com/open?id=', 'market://details?id=com.physique.univ&')});
 
-$('a#mob').each(function(){ this.href=this.href.replace('https://docs.google.com/open?id=', 'https://drive.google.com/open?id=')});
+   window.onload = function() {
+     Swal.fire({
+  icon: 'error',
+  title: '<a href="market://details?id=com.physique.univ" style=" font-family:exo 2;color: #000; ">Oops...</a>',
+  text: 'Desole, cette version a rencontre de nombreuses erreurs, tres peu de documents. Veuillez mettre a jour vers la derniere version pour continuer à utiliser l"application. Merci',
+  footer: '<a href="market://details?id=com.physique.univ" style="font-size: large;color: #f00; ">Cliquez ici pour mettre à jour</a>'
+})
+    } 
