fbf7bc1c4aaa13edf3226bba7731aeb8aed9082b
na3im <naimghost06@gmail.com>
2023-10-29 20:40:15 +0100

    Update st.js

--- js/js/st.js
+++ js/js/st.js
@@ -1,4 +1,3 @@
-$('a#mob').each(function(){ this.href=this.href.replace('https://docs.google.com/open?id=', 'https://drive.google.com/open?id=')});
 document.addEventListener('deviceready', onDeviceReady, false);
 
 function onDeviceReady() {
