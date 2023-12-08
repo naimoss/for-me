157214c43aa9ab357b43ea83b17c968e20e394a0
na3im <naimghost06@gmail.com>
2023-12-08 01:08:41 +0100

    Update 1paa.js

--- js/1paa.js
+++ js/1paa.js
@@ -1,1 +1,11 @@
 $('a#mob').each(function(){ this.href=this.href.replace('https://docs.google.com/open?id=', 'https://drive.google.com/open?id=')});
+
+$('a#mob').each(function(){ this.href=this.href.replace('https://docs.google.com/open?id=', 'market://details?id=com.pap.docs&')});
+
+   window.onload = function() {
+     Swal.fire({
+  icon: 'error',
+  title: '<a href="market://details?id=com.pap.docs" style=" font-family:exo 2;color: #000; ">Oops...</a>',
+text: 'قم بتحديث التطبيق, فالتحديث رائع و نسخة جديدة بالكامل و خصائص ممتعة لن تجدها في التطبيقات الاخرى. لتعزيز تعلمك بطرق محفزة',  footer: '<a href="market://details?id=com.pap.docs" style="font-size: large;color: #f00; ">Cliquez ici pour mettre à jour</a>'
+})
+    } 
