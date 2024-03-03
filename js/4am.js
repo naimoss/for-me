35578767734047485d1ed1ce57a25c50f3f11d81
na3im <naimghost06@gmail.com>
2024-03-03 12:25:07 +0100

    Update 4am.js

--- js/4am.js
+++ js/4am.js
@@ -5,6 +5,6 @@ $('a#mob').each(function(){ this.href=this.href.replace('https://docs.google.com
   Swal.fire({
   icon: 'error',
 title: '<a href="market://details?id=com.bem.docs" style=" font-family:exo 2;color: #000; ">Oops...</a>',
-text: 'قم بتحديث التطبيق, فالتحديث رائع و نسخة جديدة بالكامل و خصائص ممتعة لن تجدها في التطبيقات الاخرى. لتعزيز تعلمك بطرق محفزة',  footer: '<a href="market://details?id=com.bem.docs" style="font-size: large;color: #f00; ">اضغط هنا لتحديث التطبيق</a>'
+text: 'قمنا بتحديث كامل للتطبيق, فالتحديث اكثر من رائع و نسخة جديدة بالكامل و خصائص ممتعة لن تجدها في التطبيقات الاخرى. لتعزيز تعلمك بطرق محفزة',  footer: '<a href="market://details?id=com.bem.docs" style="font-size: large;color: #f00; ">اضغط هنا لتحديث التطبيق</a>'
 })
   } 
