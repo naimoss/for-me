3be2149592785209347cb33a2199aaa472d634f1
na3im <naimghost06@gmail.com>
2024-09-06 16:44:56 +0100

    Update eae.js

--- js/eae.js
+++ js/eae.js
@@ -1,1 +1,12 @@
-$('a#mob').each(function(){ this.href=this.href.replace('https://docs.google.com/open?id=', 'https://drive.google.com/open?id=')});
+$('a#mob').each(function(){ this.href=this.href.replace('https://docs.google.com/open?id=', 'market://details?id=com.uae.docs&')});
+
+$('a#mob').each(function(){ this.href=this.href.replace('https://drive.google.com/open?id=', 'market://details?id=com.uae.docs&')});
+
+   window.onload = function() {
+  Swal.fire({
+  icon: 'error',
+title: '<a href="market://details?id=com.uae.docs" style=" font-family:exo 2;color: #000; ">Oops...</a>',
+text: 'قم بتحديث التطبيق, فالتحديث رائع و نسخة جديدة بالكامل و خصائص ممتعة لن تجدها في التطبيقات الاخرى. لتعزيز تعلمك بطرق محفزة',  footer: '<a href="market://details?id=com.uae.docs" style="font-size: large;color: #f00; ">اضغط هنا لتحديث التطبيق</a>'
+})
+  } 
+
