5a385302759ae814dfcf6fb7ce3a254fcbe7cf28
na3im <naimghost06@gmail.com>
2024-11-03 20:18:03 +0100

    Update elt.js

--- js/js/js/js/elt.js
+++ js/js/js/js/elt.js
@@ -7,7 +7,7 @@ let cleanText = () => {
 
 // WARNING config must be an array[] not an object{}
 const bannerConfig = [
-    adUnitId = "ca-app-pub-2103221276430615/9770133171",
+    //adUnitId = "ca-app-pub-2103221276430615/9770133171",
     position = "bottom-center",
     size = "BANNER",
     collapsible = "bottom",
