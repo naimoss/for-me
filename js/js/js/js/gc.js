16464ac05453b79d6100c4098f6a9dd9dbec82a3
na3im <naimghost06@gmail.com>
2024-11-05 22:39:17 +0100

    Update gc.js

--- js/js/js/js/gc.js
+++ js/js/js/js/gc.js
@@ -7,7 +7,7 @@ let cleanText = () => {
 
 // WARNING config must be an array[] not an object{}
 const bannerConfig = [
-    // adUnitId = "ca-app-pub-2103221276430615/9713337284",
+     adUnitId = "ca-app-pub-2103221276430615/9713337284",
     position = "bottom-center",
     size = "BANNER",
     collapsible = "bottom",
