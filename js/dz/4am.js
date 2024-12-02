4a6f6db5a331960a8e8a163c39721f69313e7120
na3im <naimghost06@gmail.com>
2024-12-02 14:43:20 +0100

    Update 4am.js

--- js/dz/4am.js
+++ js/dz/4am.js
@@ -1,4 +1,7 @@
-$('a#mob').each(function(){ this.href=this.href.replace('https://docs.google.com/open?id=', 'https://drive.google.com/open?id=')});
+
+  $('a').each(function(){ this.href=this.href.replace('https://docs.google.com/open?id=', 'https://drive.google.com/open?id=')});
+  Array.from(document.querySelectorAll('div.docs-category-2-info>a')).filter((el,i)=>i%2===1).forEach(el=>el.href=el.previousElementSibling.href.replace("https://drive.google.com/open?id=", "https://www.univdocs.com/p/apps.html?"))
+$('a#mob').each(function(){ this.href=this.href.replace('https://docs.google.com/open?id=', 'page.html?=')});
 let event = document.getElementById('event');
 
 let cleanText = () => {
