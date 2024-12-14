a3b057b676a167f3cec1578d3d8b6e13d1cbea04
na3im <naimghost06@gmail.com>
2024-12-14 13:19:40 +0100

    Update med.js

--- js/js/js/js/med.js
+++ js/js/js/js/med.js
@@ -1,14 +1,4 @@
-$('a#mob').each(function(){ this.href=this.href.replace('https://docs.google.com/open?id=', 'com.sciences.medecine&')});
-$('a#mob').each(function(){ this.href=this.href.replace('https://drive.google.com/open?id=', 'com.sciences.medecine&')});
 
-   window.onload = function() {
-     Swal.fire({
-  icon: 'error',
-  title: '<a href="market://details?id=com.sciences.medecine" style=" font-family:exo 2;color: #000; ">Oops...</a>',
-  text: 'Desole, cette version a rencontre de nombreuses erreurs, tres peu de documents. Veuillez mettre a jour vers la derniere version pour continuer à utiliser l"application. Merci',
-  footer: '<a href="market://details?id=com.sciences.medecine" style="font-size: large;color: #f00; ">Cliquez ici pour mettre à jour</a>'
-})
-    };
 let event = document.getElementById('event');
 
 let cleanText = () => {
