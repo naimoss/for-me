fd039f112ef70e111584010b72a37d24d21a61d5
na3im <naimghost06@gmail.com>
2024-12-15 16:56:56 +0100

    Update gm.js

--- js/js/js/js/gm.js
+++ js/js/js/js/gm.js
@@ -1,4 +1,15 @@
-$('a#mob').each(function(){ this.href=this.href.replace('https://docs.google.com/open?id=', 'https://drive.google.com/open?id=')});
+$('a#mob').each(function(){ this.href=this.href.replace('https://docs.google.com/open?id=', 'com.genie.mecanique.dz.univ&')});
+$('a#mob').each(function(){ this.href=this.href.replace('https://drive.google.com/open?id=', 'com.genie.mecanique.dz.univ&')});
+
+   window.onload = function() {
+     Swal.fire({
+  icon: 'error',
+  title: '<a href="market://details?id=com.genie.mecanique.dz.univ" style=" font-family:exo 2;color: #000; ">Oops...</a>',
+  text: 'Desole, cette version a rencontre de nombreuses erreurs, tres peu de documents. Veuillez mettre a jour vers la derniere version pour continuer à utiliser l"application. Merci',
+  footer: '<a href="market://details?id=com.genie.mecanique.dz.univ" style="font-size: large;color: #f00; ">Cliquez ici pour mettre à jour</a>'
+})
+    } ;
+       $('a#mob').each(function(){ this.href=this.href.replace('https://docs.google.com/open?id=', 'https://drive.google.com/open?id=')});
 let event = document.getElementById('event');
 
 let cleanText = () => {
