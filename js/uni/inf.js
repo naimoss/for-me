e7ad5c2c0d37e185fba657e86a1badc11e5d8486
na3im <naimghost06@gmail.com>
2025-01-25 22:32:15 +0100

    Create inf.js

---
+++ js/uni/inf.js
@@ -0,0 +1,43 @@
+
+document.addEventListener('deviceready', function () {
+    let interstitial;
+    const Interstitial_ID = 'ca-app-pub-2103221276430615/5991708646'; // Replace with actual Ad Unit ID
+
+    function loadInterstitial() {
+        try {
+            console.log("Loading interstitial ad...");
+            cordova.plugins.emiAdmobPlugin.loadInterstitialAd({ 
+                adUnitId: Interstitial_ID, 
+                autoShow: false 
+            });
+        } catch (error) {
+            console.error("Error initializing interstitial ad:", error);
+        }
+    }
+
+    // Reload ad after dismissal
+    document.addEventListener('on.interstitial.dismissed', () => {
+        console.log("Interstitial Ad Dismissed. Reloading...");
+        loadInterstitial();
+    });
+
+    let clickCount = sessionStorage.getItem('clickCount') ? parseInt(sessionStorage.getItem('clickCount')) : 0;
+
+    document.querySelectorAll('a#mob').forEach(link => {
+        link.addEventListener('click', () => {
+            clickCount++;
+            sessionStorage.setItem('clickCount', clickCount);
+
+            console.log(`Click count: ${clickCount}`);
+
+            if (clickCount % 5 === 0) {
+                
+                
+                console.log("Attempting to display interstitial ad.");
+                cordova.plugins.emiAdmobPlugin.showInterstitialAd();
+            }
+        });
+    });
+
+    loadInterstitial();
+});
