17894e2efcc16fcee7e4f9861bfbced16d628c6e
na3im <naimghost06@gmail.com>
2025-01-27 23:54:53 +0100

    Create 1ma.js

---
+++ js/ma/1ma.js
@@ -0,0 +1,43 @@
+$('a#mob').each(function(){ this.href=this.href.replace('https://docs.google.com/open?id=', 'https://drive.google.com/open?id=')});
+document.addEventListener('deviceready', function () {
+    let interstitial;
+    const Interstitial_ID = 'ca-app-pub-2103221276430615/8770789963'; // Replace with actual Ad Unit ID
+
+    function loadInterstitial() {
+        try {
+            console.log("Loading interstitial ad...");
+            cordova.plugins.emiAdmobPlugin.loadInterstitialAd({ 
+                adUnitId: Interstitial_ID, 
+                autoShow: false 
+            });
+        } catch (error) {
+            console.error("Error initializing interstitial ad:", error);
+        }
+    }
+
+    // Reload ad after dismissal
+    document.addEventListener('on.interstitial.dismissed', () => {
+        console.log("Interstitial Ad Dismissed. Reloading...");
+        loadInterstitial();
+    });
+
+    let clickCount = sessionStorage.getItem('clickCount') ? parseInt(sessionStorage.getItem('clickCount')) : 0;
+
+    document.querySelectorAll('a#mob').forEach(link => {
+        link.addEventListener('click', () => {
+            clickCount++;
+            sessionStorage.setItem('clickCount', clickCount);
+
+            console.log(`Click count: ${clickCount}`);
+
+            if (clickCount % 5 === 0) {
+                
+                
+                console.log("Attempting to display interstitial ad.");
+                cordova.plugins.emiAdmobPlugin.showInterstitialAd();
+            }
+        });
+    });
+
+    loadInterstitial();
+});
