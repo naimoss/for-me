87de337f2a793304a1daa1dcb2d197f2095557e7
na3im <naimghost06@gmail.com>
2025-02-02 00:48:00 +0100

    Update 2am.js

--- js/al/2am.js
+++ js/al/2am.js
@@ -1,4 +1,4 @@
-$('a').each(function(){ this.href=this.href.replace('https://docs.google.com/open?id=', 'https://drive.google.com/open?id=')});
+$('a#mob').each(function(){ this.href=this.href.replace('https://docs.google.com/open?id=', 'https://drive.google.com/open?id=')});
 
 document.addEventListener('deviceready', function () {
     let interstitial;
