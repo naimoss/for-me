05d232d5014cc7c388b1836a3dd00aa9d9eb8275
na3im <naimghost06@gmail.com>
2025-02-02 00:14:52 +0100

    Update 3am.js

--- js/dz/3am.js
+++ js/dz/3am.js
@@ -1,2 +1,11 @@
- Array.from(document.querySelectorAll('div.docs-category-2-info>a')).filter((el,i)=>i%2===1).forEach(el=>el.href=el.previousElementSibling.href.replace("https://docs.google.com/open?id=", "https://www.univdocs.com/p/apps.html?"));
-$('a#mob').each(function(){ this.href=this.href.replace('https://docs.google.com/open?id=', 'page.html?=')});
+
+$('a#mob').each(function(){ this.href=this.href.replace('https://docs.google.com/open?id=', 'market://details?id=com.tam.docs&')});
+$('a#mob').each(function(){ this.href=this.href.replace('https://drive.google.com/open?id=', 'market://details?id=com.tam.docs&')});
+
+   window.onload = function() {
+  Swal.fire({
+  icon: 'error',
+title: '<a href="market://details?id=com.tam.docs" style=" font-family:exo 2;color: #000; ">Oops...</a>',
+text: 'قم بتحديث التطبيق, فالتحديث رائع و نسخة جديدة بالكامل و خصائص ممتعة لن تجدها في التطبيقات الاخرى. لتعزيز تعلمك بطرق محفزة',  footer: '<a href="market://details?id=com.tam.docs" style="font-size: large;color: #f00; ">اضغط هنا لتحديث التطبيق</a>'
+})
+  } 
