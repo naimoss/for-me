febb480804f6b7f13c9607aff28a3a7b4031cf11
na3im <naimghost06@gmail.com>
2025-02-12 13:08:20 +0100

    Create 5ap.js

---
+++ js/dz/5ap.js
@@ -0,0 +1,9 @@
+$('a#mob').each(function(){ this.href=this.href.replace('https://docs.google.com/open?id=', 'market://details?id=com.cap.docs&')});
+
+   window.onload = function() {
+  Swal.fire({
+  icon: 'error',
+title: '<a href="market://details?id=com.cap.docs" style=" font-family:exo 2;color: #000; ">Oops...</a>',
+text: 'قم بتحديث التطبيق, فالتحديث رائع و نسخة جديدة بالكامل و خصائص ممتعة لن تجدها في التطبيقات الاخرى. لتعزيز تعلمك بطرق محفزة',  footer: '<a href="market://details?id=com.cap.docs" style="font-size: large;color: #f00; ">اضغط هنا لتحديث التطبيق</a>'
+})
+  } 
