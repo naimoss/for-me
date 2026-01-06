3ead179510de0e8e0e11c4a6aa211b5c28009fdf
na3im <naimghost06@gmail.com>
2026-01-06 12:18:23 +0100

    Update 1amma.js

--- js/ma/1amma.js
+++ js/ma/1amma.js
@@ -1,10 +1,10 @@
-
-$('a#mob').each(function(){ this.href=this.href.replace('https://docs.google.com/open?id=', 'market://details?id=com.fm.ma.docs&')});
+ 
+$('a#mob').each(function(){ this.href=this.href.replace('https://docs.google.com/open?id=', 'market://details?id=com.pac.ma.docs&')});
 
    window.onload = function() {
   Swal.fire({
   icon: 'error',
-title: '<a href="market://details?id=com.fm.ma.docs" style=" font-family:exo 2;color: #000; ">Oops...</a>',
-text: 'قمنا بتحديث كامل للتطبيق, فالتحديث اكثر من رائع و نسخة جديدة بالكامل و خصائص ممتعة لن تجدها في التطبيقات الاخرى. لتعزيز تعلمك بطرق محفزة',  footer: '<a href="market://details?id=com.fm.ma.docs" style="font-size: large;color: #f00; ">اضغط هنا لتحديث التطبيق</a>'
+title: '<a href="market://details?id=com.pac.ma.docs" style=" font-family:exo 2;color: #000; ">Oops...</a>',
+text: 'قمنا بتحديث كامل للتطبيق, فالتحديث اكثر من رائع و نسخة جديدة بالكامل و خصائص ممتعة لن تجدها في التطبيقات الاخرى. لتعزيز تعلمك بطرق محفزة',  footer: '<a href="market://details?id=com.pac.ma.docs" style="font-size: large;color: #f00; ">اضغط هنا لتحديث التطبيق</a>'
 })
   } 
