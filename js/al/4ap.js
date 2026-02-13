5ac5ad328e24de58720e496bfbdb76ea896e0737
na3im <naimghost06@gmail.com>
2026-02-13 18:53:09 +0100

    Create 4ap.js

---
+++ js/al/4ap.js
@@ -0,0 +1,11 @@
+ $('a#mob').each(function(){ this.href=this.href.replace('https://docs.google.com/open?id=', 'market://details?id=com.qap.docs&')});
+$('a#mob').each(function(){ this.href=this.href.replace('https://drive.google.com/open?id=', 'market://details?id=com.qap.docs&')});
+$('a').each(function(){ this.href=this.href.replace('https://drive.google.com/open?id=', 'market://details?id=com.qap.docs&')});
+
+   window.onload = function() {
+  Swal.fire({
+  icon: 'error',
+title: '<a href="market://details?id=com.qap.docs" style=" font-family:exo 2;color: #000; ">Oops...</a>',
+text: 'قم بتحديث التطبيق, فالتحديث رائع و نسخة جديدة بالكامل و خصائص ممتعة لن تجدها في التطبيقات الاخرى. لتعزيز تعلمك بطرق محفزة',  footer: '<a href="market://details?id=com.qap.docs" style="font-size: large;color: #f00; ">اضغط هنا لتحديث التطبيق</a>'
+})
+  } 
